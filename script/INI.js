1e6b744a318ed80e1ced5500f163a6e40df549df
Brian <Brian151@users.noreply.github.com>
2016-03-12 17:33:56 -0700

    Added files via upload
    Whitespace remover is now in its own function, and supports tabs.
    Commented all debugging lines out
    
    [re-write complete!]

--- script/INI.js
+++ script/INI.js
@@ -1,11 +1,12 @@
 //INI.JS
 //by ETXAlienRobot201
-//Copyright (C) Crystalien Redux Project 2015
+//Copyright (C) Crystalien Redux Project 2015 - 2016
 
-var counter=0;
+
+var counter = 0;
 
 function INI(name,notStrict){
-    this.sections = new Array();
+    this.sections = [];
 	this.name = name; //only useful if INI object is stored in an array
 	this.strict = false;
 	if (!notStrict){
@@ -13,98 +14,78 @@ function INI(name,notStrict){
 	}
 	
 	this.createSection = function(index){
-		this.sections[index] = new Array();
-		this.sections[index][0] = "main"; //default section header
-		this.sections[index][1] = new Array(); //key
-		this.sections[index][2] = new Array(); //value
+		var tempS = [];
+		this.sections.push(tempS);
+		tempS.push("main"); //default section header
+		tempS.push([]); //key
+		tempS.push([]); //value
 	}
 	
-	this.findValue = function(section,key,index){
-		var out="data_missing";
-		if (!index){
-			for (var i=0; i < this.sections.length; i++){
-				if(this.sections[i][0]==section){
-					for (var i2=0; i2 < this.sections[i][1].length; i2++){
-						if (this.sections[i][1][i2]==key){
-							out=this.sections[i][2][i2];
-							break;
-						} else {
-						continue;
+	this.findValue = function(section,key,flagDupe){
+		var out = "data_missing";
+		if (flagDupe) {
+			out = false;
+		}
+		for (var i=0; i < this.sections.length; i++){
+			if(this.sections[i][0] == section){
+				for (var i2=0; i2 < this.sections[i][1].length; i2++){
+					if (this.sections[i][1][i2] == key){
+						out = this.sections[i][2][i2];
+						if (flagDupe) {
+							out = true;
+							//console.log("DUPLICATE KEY (" + key + ") FOUND!");
 						}
+						break;
 					}
-				} else {
-				continue;
 				}
 			}
 		}
+		return out;
+	}
 	
-		if(index){
-		//WORK IN PROGRESS!!!
+	this.getSection = function(section,flagDupe) {
+		var out = ["data_missing",[],[]];
+		if (flagDupe) {
+			out = false;
 		}
-	
-	return out;
+		for (var i=0; i < this.sections.length; i++){
+			if(this.sections[i][0] == section){
+				out = this.sections[i];
+				if (flagDupe) {
+					out = true;
+					//console.log("DUPLICATE SECTION (" + section + ") FOUND!");
+				}
+				break;
+			}
+		}
+		return out;
 	}
 	
 	this.toString = function(){
-	var out="";
+	var out = "";
 		for (var i=0; i < this.sections.length; i++){
-			out+="\n\n["+this.sections[i][0]+"]";
+			out += "\n\n[" + this.sections[i][0] + "]";
 			for (var i2=0; i2 < this.sections[i][1].length; i2++){
-				out+="\n"+this.sections[i][1][i2]+"="+this.sections[i][2][i2];
+				out+= "\n" + this.sections[i][1][i2] + "=" + this.sections[i][2][i2];
 			}
 		}
 	return out;
 	}
 	
-	//strict mode methods:
-	
-	//determine if existing section with name <secName> exists
-	this.findDuplicateSections = function(secName){
-		var out=false;
-		for(var i=0; i < this.sections.length; i++){
-			if(this.sections[i][0]==secName){
-				out = true;
-				break;
-			} else {
-				continue;
-			}
-		}
-		return out;
-	}
-	
-	//determine if existing key named <keyName> exists within section <secName>
-	this.findDuplicateKeys = function(secName,keyName){
-		var out=false;
-		for(var i=0; i < this.sections.length; i++){
-			if (this.sections[i][0] == secName){
-				for (var i2=0; i2 < this.sections[i][1].length; i2++){
-					if (this.sections[i][1][i2] == keyName) {
-						out = true;
-						break;
-					} else {
-						continue;
-					}
-				}
-			} else {
-				continue;
-			}
-		}
-		return out;
-	}
-	//end strict mode checks
+	//strict mode methods merged into the data-reading methods
 	
 	//INI-writing methods:
 	
 	//add section <name>
 	this.addSection = function(name){
 		if(name){
-			var failed=false;
+			var failed = false;
 			if(this.strict){
-				failed=this.findDuplicateSections(name);
+				failed = this.getSection(name,true);
 			}
 			if(!failed){
 			this.createSection(this.sections.length);
-			this.sections[this.sections.length-1][0]=name;
+			this.sections[this.sections.length-1][0] = name;
 			}
 		}
 	}
@@ -115,14 +96,12 @@ function INI(name,notStrict){
 			if (this.sections[i][0] == secName) {
 				var failed = false;
 				if (this.strict) {
-					failed = this.findDuplicateKeys(secName,keyName);
+					failed = this.findValue(secName,keyName,true);
 				}
 				if (!failed) {
 					this.sections[i][1].push(keyName);
 					this.sections[i][2].push(keyValue);
 				}
-			} else {
-				continue;
 			}
 		}
 	}
@@ -131,14 +110,12 @@ function INI(name,notStrict){
 	this.reNameSection = function(secName,secName2) {
 		var failed = false;
 		if (this.strict){
-			failed = this.findDuplicateSections(secName2);
+			failed = this.getSection(secName2,true);
 		}
 		if (!failed){
 			for (var i=0; i < this.sections.length; i++){
 				if (this.sections[i][0] == secName) {
 					this.sections[i][0] = secName2;
-				} else {
-					continue;
 				}
 			}
 		}
@@ -150,19 +127,15 @@ function INI(name,notStrict){
 			var failed = false;
 			if (this.sections[i][0] == secName){ 
 				if (this.strict){
-					failed = this.findDuplicateKeys(secName,keyName2);
+					failed = this.findValue(secName,keyName2,true);
 				}
 				if (!failed){
 					for (var i2=0; i2 < this.sections[i][1].length; i2++) {
 						if (this.sections[i][1][i2] == keyName) {
 							this.sections[i][1][i2] = keyName2;
-						} else {
-							continue;
 						}
 					}
 				}
-			} else {
-				continue;
 			}
 		}
 	}
@@ -174,12 +147,8 @@ function INI(name,notStrict){
 					for (var i2=0; i2 < this.sections[i][1].length; i2++) {
 						if (this.sections[i][1][i2] == keyName) {
 							this.sections[i][2][i2] = value;
-					} else {
-						continue;
 					}
 				}
-			} else {
-				continue;
 			}
 		}
 	}
@@ -189,8 +158,6 @@ function INI(name,notStrict){
 		for (var i=0; i < this.sections.length; i++) {
 			if (this.sections[i][0] == secName) {
 				this.sections.splice(i,1);
-			} else {
-				continue;
 			}
 		}
 	}
@@ -203,149 +170,177 @@ function INI(name,notStrict){
 					if (this.sections[i][1][i2] == keyName) {
 						this.sections[i][1].splice(i2,1);
 						this.sections[i][2].splice(i2,1);
-					} else {
-						continue;
 					}
 				}
-			} else {
-				continue;
 			}
 		}
 	}
 	
 }
 
-function parseINI(src,name,disableStrict){
-	/*deals with carriage return line feed new lines, 
-	which otherwise could compromise the INI object's data
-	*/
-	var carRet=src.indexOf("\r");
-	if(carRet >= 0){
-	var parsing=src.split("\r\n");
-	}else{
-    var parsing=src.split("\n");
+//removes trailing whitespace, now supports tab and is in a separate utility function
+function whiteSpace(input){
+	var p = input.split("");
+	var out = "";
+	for (var i2 = p.length - 1; i2 < p.length; i2--) {
+		if ((p[i2] == " ") || (p[i2] == "\t")) {
+			p.splice(i2,1);
+			//console.log("whitespace detected!");
+		} else {
+			break;
+		}
 	}
+	out = p.join("");
+	return out;
+}
+
+function parseINI(src,name,disableStrict){
     if (!name){
-     name="INI_"+counter;
+     name = "INI_" + counter;
      counter++;
     }
     var output = new INI(name);
-    var sC=0;
-	var kC=0;
+	var sC = 0;
+	var kC = 0;
 	output.createSection(sC);
+	//console.log(JSON.stringify(output.sections));
 	if (!disableStrict){
-	var sectionNames = new Array();
-	var keyNames = new Array();
-	output.strict=true;
+	var sectionNames = [];
+	var keyNames = [];
+	output.strict = true;
 	}
-
-    for (var i=0; i < parsing.length; i++){
-     var checkSection=parsing[i].indexOf("[");
-	 var checkKey=parsing[i].indexOf("=");
-	 var lineType="ignored";
-	 if ((checkSection >=0)&&(checkKey==-1)){
-		lineType="section";
-	 } else if (checkKey >= 0){
-		lineType="key";
-	 }
-        if (lineType == "section") {
-		var sec=parsing[i].split("");
-		//section syntax validator + cleaner
-		var sec2="";
-		var lbC=0; //bracket counters
-		var rbC=0;
-		for (var i2=0; i2 < sec.length; i2++){
-			if (sec[i2]=="["){
-			lbC++;
-			} else if (sec[i2]=="]"){
-			rbC++;
-			} else {
-			sec2+=sec[i2];
+	var prev = "";
+	var next = "";
+	var pending = "";
+	var inSection = false;
+	var inKey = false;
+	var inValue = false;
+	var spacey = true;
+	var currSec = "main";
+	var currKey = "";
+	
+	for (var i = 0; i < src.length; i++) {
+		var curr = src[i];
+		if (i > 0) {
+			prev = src[i-1];
+		}
+		next = src[i+1];
+		switch(curr) {
+			case "[": {
+				if ((!inSection) && (!inKey) && (!inValue)) {
+					inSection = true;
+					spacey = true;
+					output.createSection();
+					//console.log("section!");
+					sC++;
+				} else if ((inSection) && (prev == "[")) {
+					pending += curr;
+				}
+				break;
 			}
-		}
-		if ((lbC >= 2)||(rbC >= 2)){
-			var bracketTotal = "left brackets: "+lbC+" , right brackets: "+rbC;
-			alert("extra brackets detected in section declaration: "+parsing[i]+". \nThese have been removed \n"+bracketTotal);
-		}
-		parsing[i]=sec2;
-		if (!disableStrict){
-		var match=false;
-			for (var i2=0; i2 < sectionNames.length; i2++){
-				if(sectionNames[i2]==parsing[i]){
-				match=true;
+			case "]": {
+				var endSec = false;
+				if ((next == "\r") || (next == "\n")) {
+					endSec = true;
+				}
+				if (inSection) {
+					inSection = false;
+					if ((output.sections[sC][0] == "main") && (endSec)) {
+						pending = whiteSpace(pending);
+						output.sections[sC][0] =  "" + pending;
+						currSec = "" + pending;
+						pending = "";
+						//console.log("section end!");
+					} else if (!endSec) {
+						pending += curr;
+					}
+					
+				}
+				break;
+			}
+			case "=": {
+				if ((!inSection) && (!inValue) && (inKey)) {
+					pending = whiteSpace(pending);
+					output.addKey(currSec,pending,"");
+					currKey = "" + pending;
+					inKey = false;
+					inValue = true;
+					pending = "";
+					//console.log("key end!")
+					//console.log("value!");
+					spacey = true;
+				}
+				break;
+			}
+			case "\n": {
+				if (inValue) {
+					pending = whiteSpace(pending);
+					output.changeKeyValue(currSec,currKey,pending);
+					//console.log("end value!");
+				}
+				if (inKey) {
+					pending = whiteSpace(pending);
+					output.addKey(currSec,pending);
+					output.changeKeyValue(currSec,pending,"null");
+					//console.log("POSSIBLE ERROR: UNEXPECTED KEY ENDING, VALUE SET TO NULL!");
+				}
+				inSection = false;
+				inKey = false;
+				inValue = false;
+				spacey = true;
+				pending = "";
+				break;
+			}
+			case "\r": {
+				if (inValue) {
+					pending = whiteSpace(pending);
+					output.changeKeyValue(currSec,currKey,pending);
+					//console.log("end value!");
+				}
+				if (inKey) {
+					pending = whiteSpace(pending);
+					output.addKey(currSec,pending);
+					output.changeKeyValue(currSec,pending,"null");
+					//console.log("unexpected end key");
+				}
+				inSection = false;
+				inKey = false;
+				inValue = false;
+				spacey = true;
+				pending = "";
+				break;
+			}
+			case " ": {
+				if (((!inSection) && (!inValue) && (!inKey)) || spacey) {
+				break;
 				}
 			}
-			if (!match){
-			sectionNames.push(parsing[i]);
-			}
-		}
-		if (match){
-		alert("found duplicate section: "+parsing[i]+", skipping");
-		continue;
-		}
-		if (output.sections[sC][0]=="main"){
-         output.sections[sC][0]=parsing[i]; //section header
-		 } else {
-		 sC++;
-		 kC=0;
-		 output.createSection(sC);
-		 output.sections[sC][0]=parsing[i];
-		 keyNames=0;
-		 keyNames=new Array();
-		 }
-        }
-	
-	if (lineType == "key"){
-	var data=parsing[i].split("=");
-	var key=data[0].split("");
-	//key syntax validator
-	var key2="";
-	var lbC=0; //bracket counters
-	var rbC=0;
-	for (var i2=0; i2 < key.length; i2++){
-		if (key[i2]=="["){
-		lbC++;
-		} else if (key[i2]=="]"){
-		rbC++;
-		} else {
-		key2+=key[i2];
-		}
-	}
-	if ((lbC >= 1)||(rbC >= 1)){
-		var bracketTotal = "left brackets: "+lbC+" , right brackets: "+rbC;
-		alert("brackets detected in key: "+data[0]+". \nThese have been removed \n"+bracketTotal);
-	}
-	data[0]=key2;
-	if (!disableStrict){
-		var match=false;
-			for (var i2=0; i2 < keyNames.length; i2++){
-				if(sectionNames[i2]==data[0]){
-				match=true;
+			default : {
+				pending += curr;
+				if ((!inSection) && (!inValue)) {
+					inKey = true;
+				}
+				spacey = false;
+				if (inValue && ((next == undefined) || (next == null))) {
+					pending = whiteSpace(pending);
+					output.changeKeyValue(currSec,currKey,pending);
+					//console.log("end value!");
+				} else if (inKey && ((next == undefined) || (next == null))) {
+					pending = whiteSpace(pending);
+					output.addKey(currSec,pending);
+					//console.log("end value!");
 				}
 			}
-			if (!match){
-			keyNames.push(parsing[i]);
-			} 
 		}
-		if (match){
-		alert("found duplicate key: "+data[0]+", skipping");
-		continue;
-		}
-	output.sections[sC][1][kC]=data[0];
-	output.sections[sC][2][kC]=data[1];
-	kC++;
+		//console.log("CURRENT SECTION: " + currSec);
+		//console.log("CURRENT KEY: " + currKey);
 	}
 	
-	if(lineType == "ignored"){
-		continue;
-	}
-	
-    }
 	return output;
 }
 
 function INIManager(){
-	this.INILib=new Array();
+	this.INILib = [];
 	
 	this.registerINI = function(txt,id,notStrict){
 		var temp = parseINI(txt,id,notStrict);
@@ -356,7 +351,7 @@ function INIManager(){
 		var out = -1;
 		for (var i; i < this.INILib.length; i++){
 			if (this.INILib[i].name == INIsearch){
-				out=i;
+				out = i;
 			}
 		}
 		return out;
@@ -374,7 +369,7 @@ function INIManager(){
 			}
 		}
 		if (!exists){
-			alert("ERROR! INI: "+ININame+" does not exist!");
+			//alert("ERROR! INI: " + ININame + " does not exist!");
 			return "failed!";
 		} else {
 			return out;
@@ -383,7 +378,9 @@ function INIManager(){
 
 }
 
-var test = "[[[la]\nfalala=lalalalalalalalala\n[da]\n[kitty]\n]feline[=cat\n[la]\npussy=also means cat\n[DELETE ME]\n[cool games]\ngame1=MineCraft\ngame2=Halo 4\ngame3=Arcuz\ngame4=Epic Battle Fantasy 4\ngame5=Fatcat"
+//updated to test whitspace removal
+var test = "[[[la              \t\t\t\t\t\t\t\t\t\t\t\t\t\t]\nfalala=lalalalalalalalala\n[da]\n[kitty]\n]feline[=cat\n[la]\n                      pussy      =          also means cat\n[DELETE ME]\n[cool games]\ngame1=MineCraft\ngame2=Halo 4\ngame3=Arcuz\ngame4=Epic Battle Fantasy 4\ngame5=Fatcat"
+//alert(test);
 var INIStore = new INIManager();
 INIStore.registerINI(test,"myINI");
 var test2 = INIStore.checkoutINI("myINI");
@@ -391,7 +388,9 @@ var test1 = "";
 var INI_S = test2.toString();
 test1 += "ORIGINAL:\n" + INI_S + "\n\n";
 INIStore.INILib[0].addSection("I am a new section");
+INIStore.INILib[0].addSection("I am a new section"); //cause duplicate section error!
 INIStore.INILib[0].addKey("I am a new section","New Key","a new key is added!");
+INIStore.INILib[0].addKey("I am a new section","New Key","a new key is added!"); //cause duplicate key error!
 INIStore.INILib[0].reNameSection("I am a new section","New section re-named");
 INIStore.INILib[0].reNameKey("New section re-named","New Key","re-named new key");
 INIStore.INILib[0].changeKeyValue("la","falala","Deck the halls with bows of holly, fa-la-la-la-la-la-la-la-la!");
